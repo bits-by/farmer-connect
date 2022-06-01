f92ec1f3b9220250d5edde5a5a84d0be7a833234
iamvivek <vvek2468@gmail.com>
2022-06-01 10:08:49 +0530

    update login page

--- src/comp/pages/Login.js
+++ src/comp/pages/Login.js
@@ -1,55 +1,54 @@
-import React, { Component } from "react";
+import React from "react";
 import Form from "react-bootstrap/Form";
+import {Button} from "react-bootstrap";
 import Navbar from "../Navbar";
 import "../style.css";
-import {useForm} from "react-hook-form";
 
 
 const Login = () =>{
       return (
-        <div> 
+        <>
+        <Navbar/>
+       
+        <div className="container "> 
           
-          <h5 className="title-page">Sign In</h5>
-          <div id="main-div">
-            <Form>
-              <div className="usertype">
+          
+          <div id="main-div" className="card mt-5 mx-auto">
+          <div className="card-header">
+           <p className="text-center fs-2 fw-bold">Log In</p>
+           <div className="usertype">
                 <input type="radio" value="admin" className="form-check-input me-1 " name="usertype"/>Admin
                 <input type="radio" value="user" className="form-check-input ms-3 me-1" name="usertype"/>User
                 <input type="radio" value="farmer" className="form-check-input ms-3 me-1" name="usertype"/>Farmer       
               </div>
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control
-                  type="email"
-                  className="text-name"
-                  placeholder="Enter email"
-                  // {...register ("firstName",{required:true, maxLength:10})}
-                  // {...errors.email && <p> Please enter valid Email</p>}
-                />
-              </Form.Group>
+          </div>
+          <Form className="card-body m-3"> 
 
-              <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                  type="password"
-                  className="text-name"
-                  placeholder="Password"
-                />
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
-              </Form.Group>
-              <div className="text-center ">
-                <button type="button" class="btn btn-success">
-                  Submit
-                </button>
-              </div>
-              <div className="link-tag">
-                <a href="http://localhost:3000/Registration">New User! Create Account</a>
-              </div>
-            </Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+
+          </Form.Group>
+
+          
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+          <div className="link-tag text-center text-info h6">
+                <a href="http://localhost:3000/Registration" >New User! Create Account</a>
+          </div>
+        </Form>
+
+
           </div>
         </div>
+        </>
       );
    
     }
--- src/comp/pages/Registration.js
+++ src/comp/pages/Registration.js
@@ -1,60 +1,58 @@
 import React, { PureComponent } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/esm/Button";
+import Navbar from "../Navbar";
 
 const Registration = () => {
   return (
-    <div>
-\      <h5 className="title-page">Sign Up</h5>
-      <div id="main-div">
-        <Form>
-          <Form.Group className="mb-3 " controlId="formBasicEmail">
-            <Form.Label className="text-name">Name:</Form.Label>
-            <Form.Control
-              type="text"
-              className="form-control"
-              placeholder="Enter Name"
-            />
-            <Form.Text className="text-muted"></Form.Text>
+    <>
+    <Navbar/>
+
+    <div className="container">
+     
+      <div id="main-div" className="card mt-5 mx-auto">
+        <div className="card-header">
+          <p className="text-center fs-2 fw-bold">Sign In</p>
+        </div>
+        <Form className="card-body m-3"> 
+          <Form.Group className="mb-3" controlId="">
+            <Form.Label>First Name</Form.Label>
+            <Form.Control type="text" placeholder="First Name" />       
           </Form.Group>
+
+          <Form.Group className="mb-3" controlId="">
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control type="text" placeholder="Last Name" />           
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="">
+            <Form.Label>Phone Number</Form.Label>
+            <Form.Control type="number" placeholder="Phone Number" />           
+          </Form.Group>
+
           <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label className="text-name">Email address:</Form.Label>
-            <Form.Control
-              type="email"
-              className="form-control"
-              placeholder="Enter email"
-            />
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />        
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label className="text-name">Password:</Form.Label>
-            <Form.Control
-              type="password"
-              className="form-control"
-              placeholder="Password"
-            />
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
           </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label className="text-name">ReEnter Password:</Form.Label>
-            <Form.Control
-              type="password"
-              className="form-control"
-              placeholder="Password"
-            />
-          </Form.Group>
-          <div className="text-center ">
-          <button type="button" class="btn btn-success">Submit</button>
+          
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+          <div className="link-tag text-center text-info h6">
+                <a href="http://localhost:3000/Login" >Already Account!</a>
           </div>
-          <div className="link-tag">
-
-      <a href="http://localhost:3000/Login">Already Account</a>
-      </div>
         </Form>
         
       </div>
      
     </div>
+    </>
   );
 };
 
